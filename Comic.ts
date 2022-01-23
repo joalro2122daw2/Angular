586c7d71c2a688095352b2de4021478b769cf838
joalro2122daw2 <15584556.clot@fje.edu>
2022-01-23 20:24:17 +0100

    Afegit div per a mostrar categories de comics i ocultar novetats

--- Comic.ts
+++ Comic.ts
@@ -8,12 +8,37 @@ import {
   transition,
   
 } from '@angular/animations';
+import { compileDeclareDirectiveFromMetadata } from '@angular/compiler';
 
   export interface CardData {
     //imageId: string;
     state: 'default' | 'flipped' | 'matched';
 }
 
+export class Comic{
+  autor:string ="";
+  any:number = 0;
+  imatge:string = "";
+  clicked:Function=Object;
+  data:CardData = {state:'default'};
+  seleccionat:boolean=false;
+  dades:string="";
+
+  constructor(author:string,year:number,image:string,data:string)
+  {
+    this.autor = author;
+    this.any = year;
+    this.imatge = image;
+    this.clicked = this.aparadorClicked;
+    this.dades = data;
+  }
+
+  aparadorClicked()
+  {
+    //alert("clicat");
+    this.seleccionat = !this.seleccionat;
+  }
+}
 
 @Component({
   selector: 'aplicacio',
@@ -33,11 +58,50 @@ import {
         animate('200ms')
       ])
     ])
+    ,
+    trigger('moveCenter',[
+      state('true',style({transform:'scale(2,2)'})),
+      state('false',style({transform:'scale(1,1)'}))
+    ])
+    
   ]
 })
 
 
-export class Comic {
+export class Tenda {
+
+  NovetatsVisible:boolean = true;
+  AventuresVisible:boolean = false;
+  HumorVisible:boolean = false;
+  MangaVisible:boolean = false;
+  TerrorVisible:boolean = false;
+  ThrillerVisible:boolean = false;
+  FantasticVisible:boolean = false;
+
+  llistaComics=[
+    new Comic("Fco. Ibañez",1978,"../assets/humor/botones_sacarino.jpeg","../assets/humor/Botones-data.png"),
+    /*
+    new Comic("Fco. Ibañez",2020,"../assets/humor/mortadelo_filemon.jpeg"),
+    new Comic("Jan",2019,"../assets/humor/superlopez.jpeg"),
+    new Comic("Varis",1974,"../assets/humor/TBO.gif"),
+    new Comic("Varis",2014,"../assets/humor/eljueves.jpg"),
+    new Comic("Varis",1979,"../assets/humor/elvivora.jpg"),
+    new Comic("Varis",1975,"../assets/humor/makoki.jpg"),
+    new Comic("Kim",1988,"../assets/humor/martinez.jpg"),
+    new Comic("Iva",1989,"../assets/humor/putamili.jpg"),
+    new Comic("Iva",1992,"../assets/humor/makinavaja.jpg"),
+    new Comic("Gallardo",1982,"../assets/humor/ninato.jpg"),
+    new Comic("Forges",2021,"../assets/humor/forges.jpeg"),
+    new Comic("Kino",1995,"../assets/humor/mafalda.webp"),
+    new Comic("Escobar",1973,"../assets/humor/zipizape.jpeg"),
+    new Comic("Matt Groening",1973,"../assets/humor/simpson.jpeg"),
+    new Comic("Gilbert Shelton",1977,"../assets/humor/freak.jpeg"),
+    new Comic("Jim Davis",2015,"../assets/humor/garfield.jpg"),
+    new Comic("Schulz",1993,"../assets/humor/snoopy.jpg"),
+    new Comic("Martin Adams",1998,"../assets/humor/dilbert.png"),
+    new Comic("Oscar",1997,"../assets/humor/jordicule.jpeg"),
+    */
+  ]
 
       data: CardData = {
         state: "default"
@@ -105,6 +169,64 @@ export class Comic {
         } else {
           this.data6.state = "default";
         }
+      }
+
+      
+
+      public mostrarAventures(){
+        this.AventuresVisible = true;
+        this.NovetatsVisible = false;
+        this.HumorVisible = false;
+        this.FantasticVisible = false;
+        this.MangaVisible = false;
+        this.ThrillerVisible = false;
+        this.TerrorVisible = false;
+      }
+      public mostrarHumor(){
+        this.HumorVisible = true;
+        this.AventuresVisible = false;
+        this.NovetatsVisible = false;
+        this.FantasticVisible = false;
+        this.MangaVisible = false;
+        this.ThrillerVisible = false;
+        this.TerrorVisible = false;
+      }
+      public mostrarFantastic(){
+        this.FantasticVisible = true;
+        this.HumorVisible = false;
+        this.AventuresVisible = false;
+        this.NovetatsVisible = false;
+        this.MangaVisible = false;
+        this.ThrillerVisible = false;
+        this.TerrorVisible = false;
+      }
+      public mostrarManga(){
+        this.MangaVisible = true;
+        this.FantasticVisible = false;
+        this.HumorVisible = false;
+        this.AventuresVisible = false;
+        this.NovetatsVisible = false;
+        this.ThrillerVisible = false;
+        this.TerrorVisible = false;
+      }
+      public mostrarThriller(){
+        this.ThrillerVisible = true;
+        this.MangaVisible = false;
+        this.FantasticVisible = false;
+        this.HumorVisible = false;
+        this.AventuresVisible = false;
+        this.NovetatsVisible = false;
+        this.TerrorVisible = false;
+      }
+
+      public mostrarTerror(){
+        this.TerrorVisible = true;
+        this.ThrillerVisible = false;
+        this.MangaVisible = false;
+        this.FantasticVisible = false;
+        this.HumorVisible = false;
+        this.AventuresVisible = false;
+        this.NovetatsVisible = false;
       }
 
     }
--- app.module.ts
+++ app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {Comic} from './Comic';
+import {Tenda} from './Comic';
 
 /* Prueba de slider con Angular Material */
 import {MatGridListModule} from '@angular/material/grid-list'; 
@@ -14,7 +14,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    Comic
+    Tenda
   ],
   imports: [
     BrowserModule,
@@ -25,6 +25,6 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [Comic]
+  bootstrap: [Tenda]
 })
 export class AppModule { }
