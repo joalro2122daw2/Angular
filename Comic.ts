ad6e8e232b54ae7cc7c2517946300a9153b8daf9
joalro2122daw2 <15584556.clot@fje.edu>
2022-01-30 19:48:08 +0100

    Afegida animacio per a mostrar dades de comics d'humor

--- Comic.ts
+++ Comic.ts
@@ -8,7 +8,7 @@ import {
   transition,
   
 } from '@angular/animations';
-import { compileDeclareDirectiveFromMetadata } from '@angular/compiler';
+import { compileDeclareDirectiveFromMetadata, NONE_TYPE } from '@angular/compiler';
 
   export interface CardData {
     //imageId: string;
@@ -18,24 +18,34 @@ import { compileDeclareDirectiveFromMetadata } from '@angular/compiler';
 export class Comic{
   autor:string ="";
   any:number = 0;
+  preu:number = 0;
   imatge:string = "";
   clicked:Function=Object;
   data:CardData = {state:'default'};
   seleccionat:boolean=false;
+  tenda:Tenda;
   dades:string="";
 
-  constructor(author:string,year:number,image:string,data:string)
+
+  constructor(author:string,year:number,image:string,shop:Tenda,price:number)
   {
     this.autor = author;
     this.any = year;
     this.imatge = image;
     this.clicked = this.aparadorClicked;
-    this.dades = data;
+    this.tenda = shop;
+    this.preu = price;
+    this.dades = "Autor: " + this.autor + "<br>Any: " + this.any + "<br>Preu: " + this.preu + "€"; 
   }
 
   aparadorClicked()
   {
     //alert("clicat");
+    this.seleccionat = !this.seleccionat;
+    this.tenda.comicseleccionat = !this.tenda.comicseleccionat;
+    this.tenda.contingut = this.dades;
+    if(this.tenda.comicseleccionat)
+      this.tenda.mostrarDades();
     this.seleccionat = !this.seleccionat;
   }
 }
@@ -60,8 +70,19 @@ export class Comic{
     ])
     ,
     trigger('moveCenter',[
-      state('true',style({transform:'scale(2,2)'})),
-      state('false',style({transform:'scale(1,1)'}))
+
+      state('true',style({transform:'scale(1,1) translateX(50%)'})),      
+      state('false',style({transform:'scale(0,0)'})),
+      transition('1 => 0',animate('1000ms')),
+      transition('0 => 1',animate('1000ms'))
+    ]),
+
+    trigger('border',[
+
+      state('true',style({border:'solid 10px black'})),      
+      state('false',style({border:'solid 0px black'})),
+      transition('1 => 0',animate('1000ms')),
+      transition('0 => 1',animate('1000ms'))
     ])
     
   ]
@@ -77,30 +98,30 @@ export class Tenda {
   TerrorVisible:boolean = false;
   ThrillerVisible:boolean = false;
   FantasticVisible:boolean = false;
+  comicseleccionat:boolean = false;
+  contingut:string="";
 
   llistaComics=[
-    new Comic("Fco. Ibañez",1978,"../assets/humor/botones_sacarino.jpeg","../assets/humor/Botones-data.png"),
-    /*
-    new Comic("Fco. Ibañez",2020,"../assets/humor/mortadelo_filemon.jpeg"),
-    new Comic("Jan",2019,"../assets/humor/superlopez.jpeg"),
-    new Comic("Varis",1974,"../assets/humor/TBO.gif"),
-    new Comic("Varis",2014,"../assets/humor/eljueves.jpg"),
-    new Comic("Varis",1979,"../assets/humor/elvivora.jpg"),
-    new Comic("Varis",1975,"../assets/humor/makoki.jpg"),
-    new Comic("Kim",1988,"../assets/humor/martinez.jpg"),
-    new Comic("Iva",1989,"../assets/humor/putamili.jpg"),
-    new Comic("Iva",1992,"../assets/humor/makinavaja.jpg"),
-    new Comic("Gallardo",1982,"../assets/humor/ninato.jpg"),
-    new Comic("Forges",2021,"../assets/humor/forges.jpeg"),
-    new Comic("Kino",1995,"../assets/humor/mafalda.webp"),
-    new Comic("Escobar",1973,"../assets/humor/zipizape.jpeg"),
-    new Comic("Matt Groening",1973,"../assets/humor/simpson.jpeg"),
-    new Comic("Gilbert Shelton",1977,"../assets/humor/freak.jpeg"),
-    new Comic("Jim Davis",2015,"../assets/humor/garfield.jpg"),
-    new Comic("Schulz",1993,"../assets/humor/snoopy.jpg"),
-    new Comic("Martin Adams",1998,"../assets/humor/dilbert.png"),
-    new Comic("Oscar",1997,"../assets/humor/jordicule.jpeg"),
-    */
+    new Comic("Fco. Ibañez",1978,"../assets/humor/botones_sacarino.jpeg",this,30),
+    new Comic("Fco. Ibañez",2020,"../assets/humor/mortadelo_filemon.jpeg",this,24),
+    new Comic("Jan",2019,"../assets/humor/superlopez.jpeg",this,87),
+    new Comic("Varis",1974,"../assets/humor/TBO.gif",this,100),
+    new Comic("Varis",2014,"../assets/humor/eljueves.jpg",this,28),
+    new Comic("Varis",1979,"../assets/humor/elvivora.jpg",this,120),
+    new Comic("Varis",1975,"../assets/humor/makoki.jpg",this,115),
+    new Comic("Kim",1988,"../assets/humor/martinez.jpg",this,67),
+    new Comic("Iva",1989,"../assets/humor/putamili.jpg",this,170),
+    new Comic("Iva",1992,"../assets/humor/makinavaja.jpg",this,180),
+    new Comic("Gallardo",1982,"../assets/humor/ninato.jpg",this,200),
+    new Comic("Forges",2021,"../assets/humor/forges.jpeg",this,56),
+    new Comic("Kino",1995,"../assets/humor/mafalda.webp",this,78),
+    new Comic("Escobar",1973,"../assets/humor/zipizape.jpeg",this,89),
+    new Comic("Matt Groening",1973,"../assets/humor/simpson.jpeg",this,32),
+    new Comic("Gilbert Shelton",1977,"../assets/humor/freak.jpeg",this,45),
+    new Comic("Jim Davis",2015,"../assets/humor/garfield.jpg",this,54),
+    new Comic("Schulz",1993,"../assets/humor/snoopy.jpg",this,65),
+    new Comic("Martin Adams",1998,"../assets/humor/dilbert.png",this,32),
+    new Comic("Oscar",1997,"../assets/humor/jordicule.jpeg",this,25),
   ]
 
       data: CardData = {
@@ -171,7 +192,12 @@ export class Tenda {
         }
       }
 
-      
+      public mostrarDades()
+      {
+        let div = document.getElementById("DadesComic");
+        if(div)
+          div.innerHTML = this.contingut;
+      }
 
       public mostrarAventures(){
         this.AventuresVisible = true;
